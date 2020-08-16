b0392c958dace9124d1afe7aa19acf67a437490b
Restyled.io <commits@restyled.io>
2020-08-16 02:24:17 +0000

    Restyled by isort

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,15 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
 import os
+import sys
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+import stringtopy
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -55,12 +62,6 @@ project = u"stringtopy"
 copyright = u"2016, James Tocknell"
 author = u"James Tocknell"
 
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-import stringtopy
 
 version = ".".join(stringtopy.__version__.split(".")[0:2])
 # The full version, including alpha/beta/rc tags.
