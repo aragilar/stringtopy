51061a5142ff6975eb330f1712d8bb7b382012ef
Restyled.io <commits@restyled.io>
2020-12-31 05:16:59 +0000

    Restyled by isort

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
+# read the contents of your README file
+from os import path
+
 import setuptools
 
 import versioneer
-
-# read the contents of your README file
-from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
